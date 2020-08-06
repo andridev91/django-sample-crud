df130e9c196fb338a0ed0d5d2f2bd7e8a4fc0ee1
Andry Rabialahy <andridev91@gmail.com>
2020-08-06 11:37:33 +0300

    Security

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_f#$dow1m2q(9$e^bxsu^@frq7(j4g+ld4etnhq1!$q$2oh8mm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
